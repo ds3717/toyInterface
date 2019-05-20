fc833852d0bd3f67825e4092c58f5e53de191754
lampard <ds3717@columbia.edu>
2019-05-20 00:07:02 -0400

    update

--- NLPinterface/webserver/predict_label.py
+++ NLPinterface/webserver/predict_label.py
@@ -29,20 +29,8 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid, i
     return predictions
 
 
-def predict_labels(herry_data):
-    model_data = pd.read_excel('rest_data_delimited.xlsx', encoding='utf-8', sheet_name = 'Sheet1')
-    model_data = model_data.dropna(axis=0)
-    raw_X = np.array(model_data['answer_sentence'])
-    raw_y = list(model_data['sentence_epa_spss'])
-    y = []
+def predict_labels(herry_data, from_file=True):
     EPA_list = ['<EPA1>', '<EPA2>', '<EPA3>', '<EPA4>', '<EPA5>', '<EPA6>', '<EPA7>', '<EPA9>', '<EPA12>', '<EPA13>', '<Trustworthiness>']
-    for i in raw_y:
-        temp_y = [0 for i in range(11)]
-        for j in range(11):
-            if EPA_list[j] in i:
-                temp_y[j] = 1
-        y.append(temp_y)
-    y = np.array(y)
     # load the dataset
     data = open('corpus').read()
     labels, texts = [], []
@@ -54,26 +42,44 @@ def predict_labels(herry_data):
     trainDF = pandas.DataFrame()
     trainDF['text'] = texts
     trainDF['label'] = labels
-    # train_x, valid_x, train_y, valid_y = model_selection.train_test_split(raw_X, y)
-    split = int(len(raw_X)*0.9) + 9
-    train_x = raw_X[:split]
-    train_y = y[:split]
-    valid_x = raw_X[split:]
-    valid_y = y[split:]
-
     # create a count vectorizer object
     count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
     count_vect.fit(trainDF['text'])
-
-    # transform the training and validation data using count vectorizer object
-    xtrain_count =  count_vect.transform(train_x)
-    xvalid_count =  count_vect.transform(valid_x)
-    # herry_data = pd.read_excel('harry_potter_sentence_with_raw.xlsx', encoding='utf-8')
     test_x = herry_data["answer_sentence"]
     xtest_count = count_vect.transform(test_x)
 
-    # DT on Count Vectors
-    pred = train_model(OneVsRestClassifier(DecisionTreeClassifier()), xtrain_count, train_y, xtest_count)
+    if not from_file:
+        model_data = pd.read_excel('rest_data_delimited.xlsx', encoding='utf-8', sheet_name = 'Sheet1')
+        model_data = model_data.dropna(axis=0)
+        raw_X = np.array(model_data['answer_sentence'])
+        raw_y = list(model_data['sentence_epa_spss'])
+        y = []
+        for i in raw_y:
+            temp_y = [0 for i in range(11)]
+            for j in range(11):
+                if EPA_list[j] in i:
+                    temp_y[j] = 1
+            y.append(temp_y)
+        y = np.array(y)
+        # train_x, valid_x, train_y, valid_y = model_selection.train_test_split(raw_X, y)
+        split = int(len(raw_X) * 0.9) + 9
+        train_x = raw_X[:split]
+        train_y = y[:split]
+        valid_x = raw_X[split:]
+        valid_y = y[split:]
+
+
+        # transform the training and validation data using count vectorizer object
+        xtrain_count =  count_vect.transform(train_x)
+        xvalid_count =  count_vect.transform(valid_x)
+        # herry_data = pd.read_excel('harry_potter_sentence_with_raw.xlsx', encoding='utf-8')
+
+        # DT on Count Vectors
+        pred = train_model(OneVsRestClassifier(DecisionTreeClassifier()), xtrain_count, train_y, xtest_count,
+                           from_file=from_file)
+    else:
+        pred = train_model(None, None, None, xtest_count,
+                           from_file=from_file)
     comment_ids = []
     answer_sentences = []
     sentence_locations = []
